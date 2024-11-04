2d02fb3df53212585ad8e3b5fd6eab12bdefda10
Danturador <luchenokdan@gmail.com>
2024-11-05 02:06:04 +0300

    Revert "катсцена с переходом в меню"
    This reverts commit 111dd1315d112d472d5bb113065561c5c6c7c27f.

--- Assets/_ProjectFiles/Core/Video/VideoController.cs
+++ Assets/_ProjectFiles/Core/Video/VideoController.cs
@@ -6,47 +6,40 @@ using UnityEngine.SceneManagement;
 
 namespace _ProjectFiles.Core.Video
 {
-	public class VideoController : MonoBehaviour
-	{
-		private VideoPlayer _videoPlayer;
-		private SoundService _soundService;
+    public class VideoController : MonoBehaviour
+    {
+        private VideoPlayer _videoPlayer;
+        private SoundService _soundService;
+        
+        private void Start()
+        {
+            _videoPlayer = GetComponent<VideoPlayer>();
+            _videoPlayer.Prepare();
+            _videoPlayer.loopPointReached += TurnOffVideo;
+            _soundService = ServiceLocator.GetService<SoundService>();
+            UpdateVolume(_soundService.Volume);
+            _soundService.OnValueUpdated += UpdateVolume;
+        }
 
-		private void Start()
-		{
-			_videoPlayer = GetComponent<VideoPlayer>();
-			_videoPlayer.Prepare();
-			_videoPlayer.loopPointReached += TurnOffVideo;
-			_soundService = ServiceLocator.GetService<SoundService>();
-			UpdateVolume(_soundService.Volume);
-			_soundService.OnValueUpdated += UpdateVolume;
-		}
+        private void OnEnable()
+        {
+            if (_soundService is null)
+                return;
+            
+            _soundService.OnValueUpdated += UpdateVolume;
+        }
 
-		private void OnEnable()
-		{
-			if (_soundService is null)
-				return;
+        private void OnDisable()
+        {
+            _soundService.OnValueUpdated -= UpdateVolume;
+        }
+        
+        private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
 
-			_soundService.OnValueUpdated += UpdateVolume;
-		}
-
-		private void OnDisable()
-		{
-			_soundService.OnValueUpdated -= UpdateVolume;
-		}
-
-		private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
-
-		private void TurnOffVideo(VideoPlayer player)
-		{
-			if (_videoPlayer.clip.name == "катсцена 2")
-			{
-				SceneManager.LoadScene(0);
-			}
-			else
-			{
-				gameObject.SetActive(false);
-				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-			}
-		}
-	}
+        private void TurnOffVideo(VideoPlayer player)
+        {
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }
