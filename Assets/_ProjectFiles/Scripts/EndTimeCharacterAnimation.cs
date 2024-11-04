485fabcec0fb1cf8d7408281b9a2dd6d5a5febbe
Danturador <luchenokdan@gmail.com>
2024-11-05 02:05:57 +0300

    Reapply "Merge branch 'master' of https://github.com/van1ove/Jam"
    This reverts commit 7034cebe92b350df634d9ebbeee1464575f5f72e.

--- Assets/_ProjectFiles/Scripts/EndTimeCharacterAnimation.cs
+++ Assets/_ProjectFiles/Scripts/EndTimeCharacterAnimation.cs
@@ -1,26 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndTimeCharacterAnimation : MonoBehaviour
 {
     private UserInput _userInput;
     private CharacterMovement _characterMovement;
     private Rigidbody2D _rigidbody2d;
+    private CharacterContainer _characterContainer;
     void Start()
     {
         _userInput = GetComponent<UserInput>();
         _characterMovement = GetComponent<CharacterMovement>();
+        _characterContainer = GetComponent<CharacterContainer>();
         _rigidbody2d = GetComponent<Rigidbody2D>();
         GameManager.onTeleportPlayer += EndTime;
     }
 
     private void EndTime()
     {
-        _rigidbody2d.transform.position = new Vector3(14f, -7f, 0); // Тут захардкодил координаты, чтобы не прокидывать трансформ и потенциально не поломать персонажа.(можно переделать).
-        _userInput.enabled = false;
-        _characterMovement.enabled = false;
+        _rigidbody2d.transform.position = new Vector3(14f, -7f, 0);
+        //_userInput.enabled = false;
+        Debug.Log("disabled");
+        _characterMovement.Enabled = false;
         _rigidbody2d.velocity = Vector3.right;
         GameManager.onTeleportPlayer -= EndTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Exit")
+        {
+            GameManager.CurrentInventory = _characterContainer._inventory;
+            SceneManager.LoadScene("Dan");
+            Debug.Log("exit");
+        }
     }
 }
