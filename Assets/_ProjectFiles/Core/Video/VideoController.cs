58b7a1ccac6b94ba141922abbf6509a23887d9fb
Danturador <luchenokdan@gmail.com>
2024-11-05 02:05:54 +0300

    Reapply "катсцена с переходом в меню"
    This reverts commit d0bdb6323562cbd65360e434f9beffe8610c4a83.

--- Assets/_ProjectFiles/Core/Video/VideoController.cs
+++ Assets/_ProjectFiles/Core/Video/VideoController.cs
@@ -6,40 +6,47 @@ using UnityEngine.SceneManagement;
 
 namespace _ProjectFiles.Core.Video
 {
-    public class VideoController : MonoBehaviour
-    {
-        private VideoPlayer _videoPlayer;
-        private SoundService _soundService;
-        
-        private void Start()
-        {
-            _videoPlayer = GetComponent<VideoPlayer>();
-            _videoPlayer.Prepare();
-            _videoPlayer.loopPointReached += TurnOffVideo;
-            _soundService = ServiceLocator.GetService<SoundService>();
-            UpdateVolume(_soundService.Volume);
-            _soundService.OnValueUpdated += UpdateVolume;
-        }
+	public class VideoController : MonoBehaviour
+	{
+		private VideoPlayer _videoPlayer;
+		private SoundService _soundService;
 
-        private void OnEnable()
-        {
-            if (_soundService is null)
-                return;
-            
-            _soundService.OnValueUpdated += UpdateVolume;
-        }
+		private void Start()
+		{
+			_videoPlayer = GetComponent<VideoPlayer>();
+			_videoPlayer.Prepare();
+			_videoPlayer.loopPointReached += TurnOffVideo;
+			_soundService = ServiceLocator.GetService<SoundService>();
+			UpdateVolume(_soundService.Volume);
+			_soundService.OnValueUpdated += UpdateVolume;
+		}
 
-        private void OnDisable()
-        {
-            _soundService.OnValueUpdated -= UpdateVolume;
-        }
-        
-        private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
+		private void OnEnable()
+		{
+			if (_soundService is null)
+				return;
 
-        private void TurnOffVideo(VideoPlayer player)
-        {
-            gameObject.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
+			_soundService.OnValueUpdated += UpdateVolume;
+		}
+
+		private void OnDisable()
+		{
+			_soundService.OnValueUpdated -= UpdateVolume;
+		}
+
+		private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
+
+		private void TurnOffVideo(VideoPlayer player)
+		{
+			if (_videoPlayer.clip.name == "катсцена 2")
+			{
+				SceneManager.LoadScene(0);
+			}
+			else
+			{
+				gameObject.SetActive(false);
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			}
+		}
+	}
 }
