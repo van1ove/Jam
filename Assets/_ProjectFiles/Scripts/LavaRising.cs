bcb81970e58c6737ca9ea2ce6f982af20abed7e1
Danturador <luchenokdan@gmail.com>
2024-11-04 16:24:54 +0300

    добавлен объект для затопления этажа, изменены спрайты таймера

---
+++ Assets/_ProjectFiles/Scripts/LavaRising.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaRising : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer; 
+    [SerializeField] private float fadeDuration = 2f;
+
+    private void Start()
+    {
+        StartCoroutine(FadeInSprite());
+    }
+
+    private IEnumerator FadeInSprite()
+    {
+        Color color = spriteRenderer.color;
+        color.a = 0;
+        spriteRenderer.color = color;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+            spriteRenderer.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        spriteRenderer.color = color;
+    }
+}
