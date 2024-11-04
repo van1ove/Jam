86fe9b66400c6e568f731bdc62d90935a0ea9384
Keige125 <NiskoromniiNL@outlook.com>
2024-11-05 01:55:47 +0300

    Lose screen upgrade(add two buttons)

--- Assets/_ProjectFiles/Scripts/EndGameButton.cs
+++ Assets/_ProjectFiles/Scripts/EndGameButton.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class EndGameButton : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-
-    public void SwitchSceneButton()
+    [SerializeField] private string sceneName1;
+    [SerializeField] private string sceneName2;
+    public void SwitchSceneButton1()
     {
-        if (sceneName != null)
+        if (sceneName1 != null)
         {
-            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadScene(sceneName1);
+        }
+    }
+    public void SwitchSceneButton2()
+    {
+        if (sceneName2 != null)
+        {
+            SceneManager.LoadScene(sceneName2);
         }
     }
 }
