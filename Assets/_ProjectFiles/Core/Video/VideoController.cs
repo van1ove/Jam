a1b26b175bf4e136e1426e47b98698e74dd7e7c3
neosInPocket <88581498+neosInPocket@users.noreply.github.com>
2024-11-05 02:33:05 +0300

    Changes

--- Assets/_ProjectFiles/Core/Video/VideoController.cs
+++ Assets/_ProjectFiles/Core/Video/VideoController.cs
@@ -1,45 +1,52 @@
 using _ProjectFiles.Core.Infrastructure.ServiceLocator;
 using _ProjectFiles.Core.Infrastructure.ServiceLocator.Services;
 using UnityEngine;
-using UnityEngine.Video;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 namespace _ProjectFiles.Core.Video
 {
-    public class VideoController : MonoBehaviour
-    {
-        private VideoPlayer _videoPlayer;
-        private SoundService _soundService;
-        
-        private void Start()
-        {
-            _videoPlayer = GetComponent<VideoPlayer>();
-            _videoPlayer.Prepare();
-            _videoPlayer.loopPointReached += TurnOffVideo;
-            _soundService = ServiceLocator.GetService<SoundService>();
-            UpdateVolume(_soundService.Volume);
-            _soundService.OnValueUpdated += UpdateVolume;
-        }
+	public class VideoController : MonoBehaviour
+	{
+		private VideoPlayer _videoPlayer;
+		private SoundService _soundService;
 
-        private void OnEnable()
-        {
-            if (_soundService is null)
-                return;
-            
-            _soundService.OnValueUpdated += UpdateVolume;
-        }
+		private void Start()
+		{
+			_videoPlayer = GetComponent<VideoPlayer>();
+			_videoPlayer.Prepare();
+			_videoPlayer.loopPointReached += TurnOffVideo;
+			_soundService = ServiceLocator.GetService<SoundService>();
+			UpdateVolume(_soundService.Volume);
+			_soundService.OnValueUpdated += UpdateVolume;
+		}
 
-        private void OnDisable()
-        {
-            _soundService.OnValueUpdated -= UpdateVolume;
-        }
-        
-        private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
+		private void OnEnable()
+		{
+			if (_soundService is null)
+				return;
 
-        private void TurnOffVideo(VideoPlayer player)
-        {
-            gameObject.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
+			_soundService.OnValueUpdated += UpdateVolume;
+		}
+
+		private void OnDisable()
+		{
+			_soundService.OnValueUpdated -= UpdateVolume;
+		}
+
+		private void UpdateVolume(float value) => _videoPlayer.SetDirectAudioVolume(0, value);
+
+		private void TurnOffVideo(VideoPlayer player)
+		{
+			if (_videoPlayer.clip.name == "�������� 2")
+			{
+				SceneManager.LoadScene(0);
+			}
+			else
+			{
+				gameObject.SetActive(false);
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			}
+		}
+	}
 }
