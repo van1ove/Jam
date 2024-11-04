7034cebe92b350df634d9ebbeee1464575f5f72e
Danturador <luchenokdan@gmail.com>
2024-11-05 02:05:21 +0300

    Revert "Merge branch 'master' of https://github.com/van1ove/Jam"
    This reverts commit ff8fbb4b8a8c56ddc1be240a753becd208f897fa, reversing
    changes made to 111dd1315d112d472d5bb113065561c5c6c7c27f.

--- Assets/_ProjectFiles/Core/Character/Scripts/CharacterContainer.cs
+++ Assets/_ProjectFiles/Core/Character/Scripts/CharacterContainer.cs
@@ -6,31 +6,14 @@ public class CharacterContainer : MonoBehaviour
     [SerializeField] private CharacterRenderer characterRenderer;
     [SerializeField] private CharacterBehaviour characterBehaviour;
     [SerializeField] private SoundsManager soundsManager;
-    [SerializeField] private Vector2 startPosition;
-    [SerializeField] private bool is2ndStage;
-    public Inventory _inventory { get; set; }
-
-    private void Start()
-    {
-        if (is2ndStage)
-        {
-            transform.position = startPosition;
-        }
-    }
+    private Inventory _inventory;
     
     public void Initialize(Inventory inventory)
     {
         characterBehaviour.OnCharacterDeath += OnCharacterDeath;
-        characterBehaviour.OnCharacterWin += OnPlayerWin;
         _inventory = inventory;
 
         soundsManager = SoundsManager.Instance;
-        Debug.Log("initialized");
-    }
-
-    private void OnPlayerWin()
-    {
-        characterMovement.Enabled = false;
     }
 
     private void OnCharacterDeath(bool isDeathFromLava)
--- Assets/_ProjectFiles/Core/Infrastructure/Bootstrap/LavaLevel/LavaLevelEntryPoint.cs
+++ Assets/_ProjectFiles/Core/Infrastructure/Bootstrap/LavaLevel/LavaLevelEntryPoint.cs
@@ -7,10 +7,6 @@ public class LavaLevelEntryPoint : MonoBehaviour
     [SerializeField] private List<LevelItemInfo> itemInfos;
     [SerializeField] private CharacterMovement characterMovement;
     [SerializeField] private CharacterContainer characterContainer;
-    [SerializeField] private CharacterBehaviour characterBehaviour;
-    [SerializeField] private GameObject winScreen;
-    [SerializeField] private GameObject loseScreen;
-    [SerializeField] private Timer timer;
 
     private void Start()
     {
@@ -18,20 +14,5 @@ public class LavaLevelEntryPoint : MonoBehaviour
         inventoryView.Initialize(GameManager.CurrentInventory);
         characterContainer.Initialize(GameManager.CurrentInventory);
         inventoryView.Interactable(true);
-        
-        characterBehaviour.OnCharacterDeathAnimationEnd += OnCharacterDeathAnimationEnd;
-        characterBehaviour.OnCharacterWin += OnCharacterWin;
-    }
-
-    private void OnCharacterDeathAnimationEnd()
-    {
-        loseScreen.gameObject.SetActive(true);
-        timer.gameObject.SetActive(false);
-    }
-
-    private void OnCharacterWin()
-    {
-        winScreen.gameObject.SetActive(true);
-        timer.gameObject.SetActive(false);
     }
 }
