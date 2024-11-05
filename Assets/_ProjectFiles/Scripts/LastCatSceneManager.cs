35c7aacb5eaf1fde1deb04932760565dc5dd2737
Danturador <luchenokdan@gmail.com>
2024-11-05 11:18:40 +0300

    выключен звук в синематике ласт

--- Assets/_ProjectFiles/Scripts/LastCatSceneManager.cs
+++ Assets/_ProjectFiles/Scripts/LastCatSceneManager.cs
@@ -6,9 +6,14 @@ using UnityEngine.Video;
 public class LastCatSceneManager : MonoBehaviour
 {
 	[SerializeField] private VideoPlayer videoPlayer;
+	[SerializeField] private AudioSource playerSource;
+	[SerializeField] private AudioSource levelSource;
 	private void Awake()
 	{
 		PlayCatScene();
+
+		playerSource.Stop();
+		levelSource.Stop();
 	}
 	public void PlayCatScene()
 	{
